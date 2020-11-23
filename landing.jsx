da6714c1029ea37c536925aae43f69e1ea308cfe
Jessica Wolff <jleahwolff@gmail.com>
2020-11-23 10:18:40 -0600

    Initial Commit

---
+++ landing.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Landing() {
+  return(
+    <div>
+      <p>Landing Page</p>
+    </div>
+  )
+}
---
+++ nav.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "./assets/rmrs-demo-nav.svg";
+
+function Nav() {
+  return(
+    <div>
+      <img src={NavBar}/>
+    </div>
+  )
+}
+
+export default Nav;
